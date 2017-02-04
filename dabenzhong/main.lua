b4a6e242996b2a44b4f81afc895aadcca8f26e20
root <zenyuhao@gmail.com>
2017-02-04 10:43:39 +0800

    test

--- dabenzhong/main.lua
+++ dabenzhong/main.lua
@@ -1,8 +1,13 @@
 local time = tonumber(string.format("%d",os.time()/3600%24+8))%24
 local count = (tonumber(args["local_time"]) or 0)%24
 local status = args["status"] or " guang!"
+
+if status == "status" then
+    status = "Duang!"
+end
+
 if time ~= count then
-    local str = ""
+    local str = "#泡饭大笨钟机器人#\r"
     for i=1,time do str = str..status end
     ff:post({ status = str })
     args["local_time"] =  tostring(time)
