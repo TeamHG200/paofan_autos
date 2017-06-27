70f07cfe277c1ca0dba4f1ad59cc46cdad857a08
root <zenyuhao@gmail.com>
2017-06-27 17:53:15 +0800

    update good night auto

--- banlv/main.lua
+++ banlv/main.lua
@@ -9,6 +9,8 @@ local reply = {
     "晚安~",
     "good night >_<",
     "早点休息",
+    "盖好被子",
+    "在上个厕所要不",
     "好梦"
 }
 
@@ -17,9 +19,10 @@ if #tl > 0 then
     for i=1,#tl do
         local status = tl[1]    
         if string.find(status["text"], "晚安") == 1
-        and status["user"]["id"] ~= args["user_id"] then
+        and status["user"]["id"] ~= args["user_id"]
+        and status["user"]["id"] ~= 'TestByTse' then
             ff:post({
-                status = "@"..status["user"]["name"].." "..reply[math.random(4)] .. "\n#泡饭关爱机器人#",
+                status = "@"..status["user"]["name"].." "..reply[math.random(6)] .. "\n#泡饭晚安机器人#",
                 reply_id = status["id"]
             })
         end
